1f8b6199447b70cf5a74551a0ed4e2d1e6f0d356
dpajaro98 <drpajaro7@misena.edu.co>
2022-09-11 14:28:42 -0500

    cambios de correcccion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
 
+import ComponenteA from './components/contactoA';
+
 function App() {
+  const contactoPrueba = {
+    nombre: 'Darien',
+    apellido: 'Pajaro Miranda',
+    email: 'dpajaro81@gmail.com',
+    conectado: false,
+  };
   return (
     <div className="App">
-      
+      <header className="App-header">
+       
+        <ComponenteA contacto={contactoPrueba} />
+      </header>
     </div>
   );
 }
